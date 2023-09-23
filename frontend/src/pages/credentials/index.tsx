66888e182ad510b8929116a65aa7126045cdf4de
tcliv <teresa.castilla@alumni.ie.edu>
2023-09-23 20:32:38 -0300

    refactor: remove unnecessary

--- frontend/src/pages/credentials/index.tsx
+++ frontend/src/pages/credentials/index.tsx
@@ -1,29 +1,13 @@
 // ** React Imports
-import { ElementType, SyntheticEvent, useState } from 'react'
+import { ElementType } from 'react'
 
 // ** MUI Imports
-import Box from '@mui/material/Box'
-import Card from '@mui/material/Card'
-import TabList from '@mui/lab/TabList'
-import TabPanel from '@mui/lab/TabPanel'
-import TabContext from '@mui/lab/TabContext'
 import { styled } from '@mui/material/styles'
-import MuiTab, { TabProps } from '@mui/material/Tab'
-
-// ** Icons Imports
-import AccountOutline from 'mdi-material-ui/AccountOutline'
-import LockOpenOutline from 'mdi-material-ui/LockOpenOutline'
-import InformationOutline from 'mdi-material-ui/InformationOutline'
-
-// ** Demo Tabs Imports
-import TabInfo from 'src/views/account-settings/TabInfo'
-import TabAccount from 'src/views/account-settings/TabAccount'
-import TabSecurity from 'src/views/account-settings/TabSecurity'
+import { Button, ButtonProps, Grid } from '@mui/material'
 
 // ** Third Party Styles Imports
 import 'react-datepicker/dist/react-datepicker.css'
 import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
-import { Button, ButtonProps, Grid } from '@mui/material'
 import { useGlobalModalsContext } from 'src/@core/context/globalModals'
 import { CreateCredentialModal } from 'src/@core/components/modals/CreateCredentialModal'
 
